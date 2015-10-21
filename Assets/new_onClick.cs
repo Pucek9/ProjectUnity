a7b3607131c3287ce8c802d2c43d84f286a76836
MihauMihau <MichalMichal@MacBook-Pro-Tadeusz.local>
2015-10-21 12:11:27 +0200

    update

---
+++ Assets/new_onClick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class new_onClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void buttonAction (string buttonName)
+	{
+		switch (buttonName) 
+		{
+		case "new_game_btn":
+			Application.LoadLevel("lvl1");
+				break;
+		case "exit_btn":
+			Application.Quit();
+			break;
+		default:
+			Debug.Log("error");
+			break; 
+		}
+
+
+	}
+}
