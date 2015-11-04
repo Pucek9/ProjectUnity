23b14023cf75033b816f36309306a40c182d9878
MihauMihau <MichalMichal@MacBook-Pro-Tadeusz.local>
2015-11-04 11:43:22 +0100

    Pausa

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PauseManager : MonoBehaviour {
+
+	public Button pause_btn;
+	public bool isPressed=false;
+	public Sprite resume;
+	public Sprite pause;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void pauseBtnAction()
+	{
+		if (!isPressed) {
+
+			Time.timeScale = 0f;
+			isPressed = true;
+			pause_btn.image.sprite = resume;
+		} 
+		else
+		{
+			Time.timeScale = 1.0f;
+			isPressed=false;
+			pause_btn.image.sprite = pause;
+
+		}
+			
+	}
+
+}
+
+
