66b6a923ba57d103c6e2e1a413e8f0aadb29e8bb
michalgruca <michal.gruca@keyboarders.com>
2015-11-17 02:36:53 +0100

    Tomeks's changes

---
+++ Assets/Skrypty/CollectableItem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SphereCollider))]
+public class CollectableItem : Itemki {
+
+	public int nastroj = 0;
+	public int oceny = 0;
+
+	protected override void zderzenie(Player gracz){
+		Debug.Log ("CollectableItem.zderzenie");
+		gracz.zmianaNastroju (nastroj);
+		gracz.zmianaOceny (oceny);
+
+		Destroy (this.gameObject);
+	}
+	protected override void OnTriggerEnter(Collider c) {
+		base.OnTriggerEnter (c);
+	}
+}
---
+++ Assets/Skrypty/Itemki.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Itemki : MonoBehaviour {
+
+	protected abstract void zderzenie(Player gracz);
+
+	protected virtual void OnTriggerEnter(Collider c) {
+		Player p = c.gameObject.GetComponent<Player> ();
+		if (p != null) {
+			zderzenie (p);
+		}
+	}
+}
